b925018f129afa3a0cb567b49c7ced79c3b246f8
Moshed-Potato <rudah03@naver.com>
2022-08-12 11:16:51 +0900

    🔥firebase metadata read, write 구현 완료

--- BE/controllers/index.js
+++ BE/controllers/index.js
@@ -1,50 +1,33 @@
-const models = require('../models');
+import { writeNftData, readAllNftData } from '../models/Firestore.js';
 
-// 예시입니다! 필요에 따라 변경 필요!
-module.exports = {
-  items: {
-    get: (req, res) => {
-      models.items.get((error, result) => {
-        if (error) {
-          res.status(500).send('Internal Server Error');
-        } else {
-          res.status(200).json(result);
-        }
-      });
-    },
-  },
-  orders: {
-    get: (req, res) => {
-      const userId = req.params.userId;
-      // TODO: 요청에 따른 적절한 응답을 돌려주는 컨트롤러를 작성하세요.
-      if(!userId){
-        return res.sendStatus(401);
+const controller = {
+  metadata: {
+    get: async (req, res) => {
+      try{
+        const result = await readAllNftData();
+        return res.status(200).send(result);
+      } catch (e) {
+        console.error(e);
+        return res.status(500).send('Server ERROR!');
       }
-      models.orders.get(userId, (error, result) => {
-        if(error){
-          console.log(error);
-          return res.status(500).send('error');
-        }
-        else{
-          return res.status(200).json(result);
-        }
-      })
     },
-    post: (req, res) => {
-      const userId = req.params.userId;
-      const { orders, totalPrice } = req.body;
-      // TODO: 요청에 따른 적절한 응답을 돌려주는 컨트롤러를 작성하세요.
-      if(!userId || !orders || !totalPrice){
-        return res.sendStatus(400);
+    post: async (req, res) => {
+      const { tokenId, name, imageUrl, description } = req.body;
+      try{
+        await writeNftData(tokenId, name, imageUrl, description);
+        return res.status(200).send("Write DONE");
+      } catch (e) {
+        console.error(e);
+        return res.status(500).send("Server ERROR!");
       }
-      models.orders.post(userId, orders, totalPrice, (error, result) => {
-        if(error){
-          return res.status(500).send('error');
-        }
-        else{
-          return res.status(201).json(result);
-        }
-      })
-    },
+    }
   },
+  // TODO!
+  minting: {
+    post: async (req, res) => {
+      
+    },
+  }
 };
+
+export { controller };
